c3bc7eb46bacc48213f709cd68e28d8746f2b7ab
Filippo Baldi <baldi.filippo@icloud.com>
2022-10-28 08:28:30 +0200

    lista aggiornata con funzione di aggiunta di preferiti

--- main.js
+++ main.js
@@ -15,35 +15,52 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function generateList() {
   const ul = document.querySelector('.list')
-  storeList.forEach((shop) => {
+  storeList.forEach((monument) => {
     const li = document.createElement('li')
     const div = document.createElement('div')
+    const starDiv = document.createElement('div')
     const a = document.createElement('a')
     const p = document.createElement('p')
+    const star = document.createElement('span')
+
+    // make the star element like <span class="material-symbols-outlined">
+    // star
+    // </span>
+
+    star.classList.add('material-symbols-outlined')
+
+    star.classList.add('star')
+    star.textContent = 'star'
+
+    div.classList.add('list-element')
+    starDiv.classList.add('star-div')
+
     a.addEventListener('click', () => {
-      flyToStore(shop)
+      flyToStore(monument)
     })
-    div.classList.add('shop-item')
-    a.innerText = shop.properties.name
+    div.classList.add('monument-item')
+    a.innerText = monument.properties.name
     a.href = '#'
-    p.innerText = shop.properties.address
+    p.innerText = monument.properties.address
 
     div.appendChild(a)
     div.appendChild(p)
     li.appendChild(div)
+    li.appendChild(starDiv)
     ul.appendChild(li)
+    starDiv.appendChild(star)
   })
 }
 
 generateList()
 
-function makePopupContent(shop) {
+function makePopupContent(monument) {
   return `
-    <div>
-        <h4>${shop.properties.name}</h4>
-        <p>${shop.properties.address}</p>
-        <div class="phone-number">
-            <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+    <div class="card" >
+        <h4>${monument.properties.name}</h4>
+        <p>${monument.properties.address}</p>
+        <div class="img">
+            <img src="${monument.properties.image}" alt="${monument.properties.name}">
         </div>
     </div>
   `
@@ -60,13 +77,18 @@ var myIcon = L.icon({
   iconSize: [30, 40],
 })
 
-const shopsLayer = L.geoJSON(storeList, {
+var favIcon = L.icon({
+  iconUrl: 'yellowMarker.png',
+  iconSize: [30, 40],
+})
+
+const monumentsLayer = L.geoJSON(storeList, {
   onEachFeature: onEachFeature,
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, { icon: myIcon })
   },
 })
-shopsLayer.addTo(myMap)
+monumentsLayer.addTo(myMap)
 
 function flyToStore(store) {
   const lat = store.geometry.coordinates[1]
@@ -81,3 +103,53 @@ function flyToStore(store) {
       .openOn(myMap)
   }, 3000)
 }
+
+const resetZoom = document.querySelector('.earth-icon')
+
+resetZoom.addEventListener('click', () => {
+  myMap.setView([0, 0], 0)
+})
+
+const star = document.querySelectorAll('.star')
+
+star.forEach((star) => {
+  star.addEventListener('click', () => {
+    star.classList.toggle('star-active')
+
+    if (star.classList.contains('star-active')) {
+      const monumentLi = star.parentElement.parentElement
+      const list = document.querySelector('.list')
+      list.insertBefore(monumentLi, list.childNodes[0])
+
+      const monumentName = monumentLi.querySelector('a').innerText
+
+      const monument = storeList.find(
+        (monument) => monument.properties.name === monumentName
+      )
+
+      const lat = monument.geometry.coordinates[1]
+      const lng = monument.geometry.coordinates[0]
+
+      //monument marker
+      const monumentMarker = L.marker([lat, lng], { icon: favIcon })
+      monumentMarker.addTo(myMap)
+    } else {
+      const monumentLi = star.parentElement.parentElement
+      const list = document.querySelector('.list')
+      list.appendChild(monumentLi)
+
+      const monumentName = monumentLi.querySelector('a').innerText
+
+      const monument = storeList.find(
+        (monument) => monument.properties.name === monumentName
+      )
+
+      const lat = monument.geometry.coordinates[1]
+      const lng = monument.geometry.coordinates[0]
+
+      //monument marker
+      const monumentMarker = L.marker([lat, lng], { icon: myIcon })
+      monumentMarker.addTo(myMap)
+    }
+  })
+})
--- stores.js
+++ stores.js
@@ -8,6 +8,8 @@ const storeList = [
     properties: {
       name: 'Colosseo di Roma',
       address: 'Piazza del Colosseo, 1, 00184 Roma RM, Italy',
+      image:
+        'https://www.italia.it/content/dam/tdh/it/destinations/lazio/roma/colosseo/media/20220104090828-colosseo-intero-roma-lazio-shutterstock-1901927272.jpg',
     },
   },
   {
@@ -19,6 +21,8 @@ const storeList = [
     properties: {
       name: 'Duomo Di Milano',
       address: 'Piazza del Duomo, 20121 Milano MI, Italy',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Milan_Cathedral_from_Piazza_del_Duomo.jpg/640px-Milan_Cathedral_from_Piazza_del_Duomo.jpg',
     },
   },
   {
@@ -30,6 +34,8 @@ const storeList = [
     properties: {
       name: 'Empire State Building',
       address: 'New York, NY 10001, Stati Uniti',
+      image:
+        'https://www.viaggi-usa.it/wp-content/uploads/2017/06/Empire-State-Building-New-York-Cosa-Vedere.jpg',
     },
   },
   {
@@ -42,6 +48,8 @@ const storeList = [
       name: 'Burj Khalifa',
       address:
         'Sheikh Mohammed bin Rashid Blvd 1  - Downtown Dubai - Dubai - Emirati Arabi Uniti',
+      image:
+        'https://static.geopop.it/wp-content/uploads/sites/32/2022/09/burj-khalifa-costruzione.jpg',
     },
   },
   {
@@ -53,6 +61,47 @@ const storeList = [
     properties: {
       name: 'Statue of Liberty',
       address: 'Liberty Island, New York, NY, 10004',
+      image:
+        'https://cdn.britannica.com/22/195522-050-6C15247F/Statue-of-Liberty-Upper-New-York-Bay.jpg',
+    },
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [2.294694, 48.858093],
+    },
+    properties: {
+      name: 'Eiffel Tower',
+      address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, Francia',
+      image:
+        'https://www.travel365.it/foto/eiffel-tower-is-famous-best-destinations-paris-france.jpg',
+    },
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [31.20889, 30.01222],
+    },
+    properties: {
+      name: 'Grande Sfinge di Giza',
+      address: 'Al Giza Desert, Governatorato di Giza 3512201, Egitto',
+      image:
+        'https://www.costacrociere.it/content/dam/costa/costa-magazine/articles-magazine/heritage/sfinge/la-grande-sfinge-di-giza_m.jpg.image.694.390.low.jpg',
+    },
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-98.30352, 19.06406],
+    },
+    properties: {
+      name: 'Piramidi mesoamericane',
+      address: 'Rio de Janeiro Brasile',
+      image:
+        'https://www.dentroriodejaneiro.it/wp-content/uploads/2018/01/cosa-fare-a-rio-de-janeiro-cristo-redentor.jpg',
     },
   },
 ]
